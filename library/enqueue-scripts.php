963f65eda6afcdde7596b329e73b0a52ea6c28ce
steelbridge-io <chris@steelbridge.io>
2019-03-29 14:05:46 -0700

    Updated

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -17,40 +17,44 @@
 if ( ! function_exists( 'lavacreek_scripts' ) ) :
 	function lavacreek_scripts() {
 
-		// Enqueue the main Stylesheet.
-		wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/dist/assets/css/' .
-            foundationpress_asset_path( 'app.css' ), array(), '2.10.4', 'all' );
-      
-		// Deregister the jquery version bundled with WordPress.
-		wp_deregister_script( 'jquery' );
-     
+        // Enqueue the main Stylesheet.
+        wp_enqueue_style('main-stylesheet', get_template_directory_uri() . '/dist/assets/css/' .
+            foundationpress_asset_path('app.css'), array(), '2.10.4', 'all');
 
-		// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-		wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', false );
+        wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
+            array(), '5.7.2', 'all');
 
-		// Deregister the jquery-migrate version bundled with WordPress.
-		wp_deregister_script( 'jquery-migrate' );
+        // Deregister the jquery version bundled with WordPress.
+        wp_deregister_script('jquery');
 
-		// CDN hosted jQuery migrate for compatibility with jQuery 3.x
-		wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array('jquery'), '3.0.1', false );
+        // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+        wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2
+	.1', true);
 
-		// Enqueue jQuery migrate. Uncomment the line below to enable.
-    wp_enqueue_script( 'jquery-migrate' );
+        wp_enqueue_script('parallax-js', get_template_directory_uri() . '/library/js/parallax.min.js', array(), '',
+            true);
 
-		// Enqueue Foundation scripts
-		wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/' . foundationpress_asset_path(
-		    'app.js' ), array( 'jquery' ), '2.10.4', true );
-     
-		//Enqueue FontAwesome from CDN. Uncomment the line below if you need FontAwesome.
-		wp_enqueue_script( 'fontawesome', 'https://use.fontawesome.com/5016a31c8c.js', array(), '4.7.0', true );
+        // Deregister the jquery-migrate version bundled with WordPress.
+        wp_deregister_script('jquery-migrate');
 
-		// Add the comment-reply library on pages where it is necessary
-		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-			wp_enqueue_script( 'comment-reply' );
-		}
-    
-    wp_enqueue_script( 'parallax', 'https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js', array(), '1.4.2', true );
-    wp_enqueue_script( 'jquery-parallax', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), '20181212', true );
+        // CDN hosted jQuery migrate for compatibility with jQuery 3.x
+        wp_register_script('jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array
+        ('jquery'), '3.0.1', true);
+
+        // Enqueue jQuery migrate. Uncomment the line below to enable.
+        wp_enqueue_script('jquery-migrate');
+
+        //Enqueue FontAwesome from CDN. Uncomment the line below if you need FontAwesome.
+        wp_enqueue_script('fontawesome', 'https://use.fontawesome.com/5016a31c8c.js', array(), '4.7.0', true);
+
+        // Enqueue Foundation scripts
+        wp_enqueue_script('foundation', get_template_directory_uri() . '/dist/assets/js/' .
+            foundationpress_asset_path('app.js'), array('jquery'), '2.10.4', true);
+
+        // Add the comment-reply library on pages where it is necessary
+        if (is_singular() && comments_open() && get_option('thread_comments')) {
+            wp_enqueue_script('comment-reply');
+        }
     
     $slider2img1 = 'main-stylesheet';  // Swap in your CSS Stylesheet ID
 
