2c6e29f614025358fa83c9f67d3d1443fd8ebc29
steelbridge-io <chris@steelbridge.io>
2019-05-28 16:58:52 -0700

    First Commit

---
+++ library/custom-nav.php
@@ -0,0 +1,10 @@
+<?php
+
+if (! function_exists('lava_creek_theme_customizer')) :
+
+    add_action('customize_register','lava_creek_theme_customizer');
+
+    function lava_creek_theme_customizer($wp_customize)
+    {
+
+    } endif;
